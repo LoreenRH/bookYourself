0928e1e59e593d49ff7671cab055f55cd6d559d4
LoreenRH <lorena.rangel@docker.com>
2018-04-17 18:39:17 +0200

    Make the appp beautiful again
    Signed-off-by: LoreenRH <lorena.rangel@docker.com>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { HttpClientModule } from '@angular/common/http';
+import { MatListModule } from '@angular/material/list';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { BookingInfoComponent } from './booking-info/booking-info.component';
+import {MatCardModule} from '@angular/material/card';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BookingInfoComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    MatListModule,
+    MatGridListModule, 
+    MatCardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/booking-info/booking-info.component.ts
+++ src/app/booking-info/booking-info.component.ts
@@ -3,13 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { HttpErrorResponse } from '@angular/common/http';
 
 interface IMeeting {
-  name: string;
-  start: string;
-  end: string;
+  title: string;
+  startDate: string;
+  endDate: string;
 }
 
 export class Room {
-  room: string;
+  roomName: string;
   currentMeeting: IMeeting;
   nextMeeting: IMeeting;
 }
@@ -27,43 +27,41 @@ export class BookingInfoComponent implements OnInit {
   }
   isAvailable = "";
   until = "";
-  nextMeeting = {};
+  nextMeeting = "";
   nextMeetingTime;
   clock;
 
   ngOnInit() {
-    this.httpService.get('../../assets/data.json')
+    this.httpService.get("../assets/data.json")
     .subscribe(
       data => {
-        this.jsonData =  data as Room;
+        this.jsonData = data as Room;
         this.RoomAvailability(this.jsonData);
-      },
-      error => {
-        console.log(error);
       }
-    );
+    )
+     
     setInterval(() => {
       this.clock = new Date().toLocaleTimeString();
     }, 1000);
   }
 
   RoomAvailability(data) {
-    var startTime = new Date(data.currentMeeting.start);
-    var endTime = new Date (data.currentMeeting.end);
+    var startTime = new Date(data.currentMeeting.startDate);
+    var endTime = new Date (data.currentMeeting.endDate);
     var now = new Date();
     var options = {hour: "numeric", minute: "numeric"};
     console.log(endTime, startTime, now)
     if (startTime < now && endTime > now){
       // actual meeting is going on
-      this.isAvailable = "Booked until : ";
+      this.isAvailable = "Booked until: ";
       this.until = endTime.toLocaleTimeString('UTC', options); 
     } else {
       // no meeting
       this.isAvailable = "Available until: ";
-      var nextMeeting = new Date(data.nextMeeting.start);
+      var nextMeeting = new Date(data.nextMeeting.startDate);
       this.until = nextMeeting.toLocaleTimeString('UTC', options);
     }
-    this.nextMeeting = data.nextMeeting;
-    this.nextMeetingTime = new Date(data.nextMeeting.start).toLocaleTimeString('UTC', options);
+    this.nextMeeting = data.nextMeeting.title;
+    this.nextMeetingTime = new Date(data.nextMeeting.startDate).toLocaleTimeString('UTC', options);
   }
 }
